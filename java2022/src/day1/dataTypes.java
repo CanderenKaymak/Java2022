8529411808839473307bf4d9e4354454fd3c23d9
CanderenKaymak <canderenkaymak@gmail.com>
2022-10-16 21:01:46 +0300

    dataTypes added for kodlama.io day one

---
+++ java2022/src/day1/dataTypes.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class dataTypes {
+    public static void main(String[] args) {
+        int number = 1231231232;
+
+        double numberTwo = -123.5;
+
+        char letter = 'A';
+
+        long longNumber = 1231231231212312112L;
+
+        byte byteNum = 127;
+
+        String stringExam = "Japonya'ya gider iken aldı da bir yağmur..";
+
+        boolean isTrue = true;
+
+        System.out.println(isTrue);
+    }
+}
