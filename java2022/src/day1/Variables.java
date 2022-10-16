ce88a2933b85655072ab22f6edb444d38f8d51c4
CanderenKaymak <canderenkaymak@gmail.com>
2022-10-16 21:27:23 +0300

    Variables added for day one

---
+++ java2022/src/day1/Variables.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Variables {
+    public static void main(String[] args) {
+        int numberOfStudents = 12;
+        String message = "Ogrenci Sayisi: ";
+
+        System.out.println(message + numberOfStudents);
+    }
+}
