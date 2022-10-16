2dbaea0c09c71160b3cefbe9a6cd14457dadb90c
CanderenKaymak <canderenkaymak@gmail.com>
2022-10-16 21:58:01 +0300

    Mini Procject - 4 Friend Numbers added for day one

---
+++ java2022/src/day1/MiniProcject4FriendNumbers.java
@@ -0,0 +1,32 @@
+package day1;
+
+public class MiniProcject4FriendNumbers {
+    public static void main(String[] args) {
+        // 220 and 284 are smallest friend numbers
+
+        int num1 = 220;
+        int num2 = 284;
+
+        int dividingTotal1 = 0;
+        int dividingTotal2 = 0;
+
+        for (int i = 1; i < num1; i++){
+            if (num1 % i == 0){
+                dividingTotal1 += i;
+            }
+        }
+
+        for (int i = 1; i < num2; i++){
+            if (num2 % i == 0){
+                dividingTotal2 += i;
+            }
+        }
+
+        if (num1 == dividingTotal2 && num2 == dividingTotal1){
+            System.out.println("These numbers are Friends.");
+        }
+        else{
+            System.out.println("These numbers are NOT Friends.");
+        }
+    }
+}
