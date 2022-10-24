1b39e276b923724b9dcf6b28be83574ee0c0183b
CanderenKaymak <canderenkaymak@gmail.com>
2022-10-24 18:29:56 +0300

    Methods intro added for day two

--- java2022/src/day2/Methods.java
+++ java2022/src/day2/Methods.java
@@ -1,2 +1,24 @@
-package day2;public class Methods {
+package day2;
+
+import java.util.Arrays;
+
+public class Methods {
+    public static void main(String[] args) {
+        // metodu cagirdik
+        findingNumber();
+    }
+
+    public static void findingNumber(){
+        // eger bunu bir metoda almadan farkli yerlerde kullansaydik, bu fonksiyon uzerinde degisiklik yapmak
+        // istedigimizde her kullandigimiz ayrı yer icin duzeltme yapmak zorunda kalirdik. bunun onune gecmek icin
+        // metotlari kullaniyoruz.
+        int[] numbers = new int[]{1,2,5,7,9,0};
+        int searching = 5;
+
+        if (!(Arrays.stream(numbers).anyMatch(value -> value == searching))) {
+            System.out.println("number Not found");
+        } else {
+            System.out.println("number is existing.");
+        }
+    }
 }
