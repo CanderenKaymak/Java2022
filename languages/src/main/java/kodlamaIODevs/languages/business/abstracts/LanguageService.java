30cdbed8a2907188e96b0de87352b75aed2cfaea
CanderenKaymak <canderenkaymak@gmail.com>
2022-12-08 19:17:15 +0300

    LanguageService interface and CRUD operations created

---
+++ languages/src/main/java/kodlamaIODevs/languages/business/abstracts/LanguageService.java
@@ -0,0 +1,13 @@
+package kodlamaIODevs.languages.business.abstracts;
+
+import kodlamaIODevs.languages.entities.concretes.Language;
+
+import java.util.List;
+
+public interface LanguageService {
+    List<Language> getAll();
+    Language getById(int id) throws Exception;
+    void delete(int id) throws Exception;
+    void update(Language language) throws Exception;
+    void create(Language language) throws Exception;
+}
