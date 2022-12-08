b3a96093115106bd24f75af80fa2e9bcb7c6b3cf
CanderenKaymak <canderenkaymak@gmail.com>
2022-12-08 20:17:38 +0300

    getAll() method is modified due to an Exception error on LanguageManager class

--- languages/src/main/java/kodlamaIODevs/languages/business/abstracts/LanguageService.java
+++ languages/src/main/java/kodlamaIODevs/languages/business/abstracts/LanguageService.java
@@ -5,7 +5,7 @@ import kodlamaIODevs.languages.entities.concretes.Language;
 import java.util.List;
 
 public interface LanguageService {
-    List<Language> getAll();
+    List<Language> getAll() throws Exception;
     Language getById(int id) throws Exception;
     void delete(int id) throws Exception;
     void update(Language language) throws Exception;
