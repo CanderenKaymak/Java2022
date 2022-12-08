a3d73d93e2b699b4ed0952316f004e9db0bed803
CanderenKaymak <canderenkaymak@gmail.com>
2022-12-08 18:20:07 +0300

    Language Data Access interface created with CRUD method bodies

--- languages/src/main/java/kodlamaIODevs/languages/dataAccess/abstracts/LanguageDao.java
+++ languages/src/main/java/kodlamaIODevs/languages/dataAccess/abstracts/LanguageDao.java
@@ -1,2 +1,13 @@
-package kodlamaIODevs.languages.dataAccess.abstracts;public interface LanguageDao {
+package kodlamaIODevs.languages.dataAccess.abstracts;
+
+import kodlamaIODevs.languages.entities.concretes.Language;
+
+import java.util.List;
+
+public interface LanguageDao {
+    List<Language> getAll() throws Exception;
+    Language getById(int id) throws Exception;
+    void delete(int id) throws Exception;
+    void update(Language language) throws Exception;
+    void create(Language language) throws Exception;
 }
